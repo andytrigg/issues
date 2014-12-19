623f683ab9ea2316cecf1143159f93fe75bb82c7
Andy Trigg <atrigg@aconex.com>
2014-12-19 15:31:58 +1100

    further fleshing out the example

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,18 @@
+defmodule Issues.GithubIssues do
+  @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"    
+  end
+
+  def handle_response({:ok, body}), do: { :ok, body }
+  def handle_response({ ___, body}), do: { :error, body }
+end
+
+
