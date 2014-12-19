7ecc930c72ef00398b0c10d6444f12eda0181ae3
Andy Trigg <atrigg@aconex.com>
2014-12-19 16:27:52 +1100

    this is a broken commit, not sure why...will figure out later

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -40,8 +40,7 @@ defmodule Issues.CLI do
   end
 
   def decode_response({:ok, body}), do: body
-
-  def decode_response({:error, body}) do
+  def decode_response({:error, error}) do
     {_, message} = List.keyfind(error, "message", 0)
     IO.puts "Error fetching from Github: #{message}"
     System.halt(2)
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -7,8 +7,11 @@ defmodule Issues.GithubIssues do
     |> handle_response
   end
 
+  # use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
+
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"    
+    "#{@github_url}/repos/#{user}/#{project}/issues"    
   end
 
   def handle_response({:ok, body}) do
