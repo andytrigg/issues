65c8ea9297b5ba5a8c77634f2d4bd643d31244c0
Andy Trigg <atrigg@aconex.com>
2014-12-22 11:02:48 +1100

    Added a heap of stuff following daves examples from the book

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -37,6 +37,8 @@ defmodule Issues.CLI do
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
+    |> convert_to_list_of_hashdicts
+    |> sort_into_ascending_order
   end
 
   def decode_response({:ok, body}), do: body
@@ -49,5 +51,9 @@ defmodule Issues.CLI do
   def convert_to_list_of_hashdicts(list) do
     list
     |> Enum.map(&Enum.into(&1, HashDict.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 end
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,24 +1,25 @@
 defmodule Issues.GithubIssues do
-  @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
-
+  
+  @user_agent  [ {"User-agent", "Elixir dave@pragprog.com"} ]
+    
   def fetch(user, project) do
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
   end
+  
+  def handle_response(%{status_code: 200, body: body}) do
+    { :ok, :jsx.decode(body) }
+  end
+  def handle_response(%{status_code: ___, body: body}) do
+    { :error, :jsx.decode(body) }
+  end
 
   # use a module attribute to fetch the value at compile time
   @github_url Application.get_env(:issues, :github_url)
-
+                                              
   def issues_url(user, project) do
-    "#{@github_url}/repos/#{user}/#{project}/issues"    
-  end
-
-  def handle_response({:ok, body}) do
-    { :ok, :jsx.decode(body) }
-  end
-  def handle_response({ ___, body}) do
-    { :error, :jsx.decode(body) }
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 end
 
